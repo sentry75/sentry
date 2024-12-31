87b0cd26d1e8908743868c91cf353313311bae72
sentry75 <orgsentry75@gmail.com>
2024-12-31 14:55:03 +0000

    update @v2.1

---
+++ data/index.ts
@@ -0,0 +1,22 @@
+export const navItems = [
+  { name: "Home", link: "#home" },
+  { name: "About", link: "#about" },
+  { name: "Blog", link: "#" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
